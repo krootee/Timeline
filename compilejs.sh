e01c5d973f53ed75f154c3b6b3376b23359e6320
Konstantin Root <konstantin.root@windowslive.com>
2012-06-23 22:29:14 +0300

    initial commit of js file compiler of all files into 1

---
+++ compilejs.sh
@@ -0,0 +1,4 @@
+cd source/js/Embed
+cat "../lib/Embed.LoadLib.js" Embed.js > "../../../timeline-embed.js"
+cd ..
+cat VMM.Timeline.License.js "Core/VMM.js" "Core/VMM.Library.js" "Core/VMM.Browser.js" "Core/VMM.FileExtention.js" "Core/VMM.Date.js" "Core/VMM.Util.js" "Core/VMM.LoadLib.js" "Media/VMM.ExternalAPI.js" "Media/VMM.MediaElement.js" "Media/VMM.MediaType.js" "Media/VMM.Media.js" "Media/VMM.TextElement.js" "Slider/VMM.TouchSlider.js" "Slider/VMM.DragSlider.js" "Slider/VMM.Slider.js" "Slider/VMM.Slider.Slide.js" VMM.Language.js "lib/AES.js" "lib/bootstrap-tooltip.js" VMM.Timeline.js VMM.Timeline.TimeNav.js VMM.Timeline.DataObj.js > "../../timeline.js"
