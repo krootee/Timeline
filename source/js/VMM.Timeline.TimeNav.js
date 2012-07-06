2b32202f6443c38844966d22679c34f5c0d563ad
minisaw <minisaw@gmail.com>
2012-07-06 17:21:56 +0300

    OCN-117 - fixed. reverted one performance tweak. turns out that the intervals information is unique each time "createIntervalElements" is called, so it is hard to cache it.
    additionally, spotted a memory leak: the arrays interval_array and interval_major_array grow in size over time.

--- source/js/VMM.Timeline.TimeNav.js
+++ source/js/VMM.Timeline.TimeNav.js
@@ -848,33 +848,7 @@ if(typeof VMM.Timeline != 'undefined' && typeof VMM.Timeline.TimeNav == 'undefin
              */
         }
 
-        function reuseMarkersCache(_element_parent, _array, _interval) {
-
-            // the stuff below is basically a copy/paste of the createIntervalElements function body
-            VMM.attachElement(_element_parent, "");
-
-            for (var i = 0; i < _array.length; i++) {
-                var _element = VMM.appendAndGetElement(_element_parent, "<div>", _interval.classname);
-
-                VMM.appendElement(_element, _array[i].pretty_date);
-
-                _array[i].interval_element = _element;
-
-                // OCN-107: the following line has been commented out. it is very CPU consuming
-                // OCN-107: the effect is that the marker text (e.g. JULY 4) is not centered around its marker
-                //VMM.Lib.css(_element, "text-indent", -(VMM.Lib.width(_element)/2));
-                //VMM.Lib.css(_element, "opacity", "0");
-            }
-
-            positionInterval(_element_parent, _array);
-        }
-
         var createIntervalElements = function(_interval, _array, _element_parent) {
-
-            // OCN-107: re-use the already-built markers, if available
-            if (_array.length != 0) {
-                return reuseMarkersCache(_element_parent, _array, _interval);
-            }
 
             // OCN-107: limit the max number of markers to MAX_INTERVALS_COUNT
             var timeIncrement = 1;
